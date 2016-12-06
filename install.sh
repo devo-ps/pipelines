0ff95f1579d6133d1f7a78d5ad3f0b337918d39f
xuqingfeng <js-xqf@hotmail.com>
2016-12-06 11:56:09 +0800

    Improve install.sh

--- install.sh
+++ install.sh
@@ -6,34 +6,33 @@ install_pipelines(){
 
     echo "Checking dependencies..."
     if [ ! -n "$(which python)" ]; then
-        echo "Need python !";
+        echo "Need python installed !";
         exit 1;
     fi
     if [ ! -n "$(which pip)" ]; then
-        echo "Need pip !";
+        echo "Need pip installed !";
         exit 1;
     fi
     if [ ! -n "$(which git)" ]; then
-        echo "Need git !"
+        echo "Need git installed !"
         exit 1;
     fi
 
     echo "Installing pipelines..."
-    pip_install="pip install git+git://github.com/Wiredcraft/pipelines@dev"
-    eval $pip_install
+    sudo pip install git+git://github.com/Wiredcraft/pipelines@dev > /dev/null
 
     echo "Creating user..."
-    user=`getent passwd pipelines > /dev/null`
-    if [ $user -eq 0 ]; then
-
-        `useradd -m -d /var/lib/pipelines -s /bin/false pipelines`
+    id pipelines > /dev/null 2>&1
+    if [ ! $? -eq 0 ]; then
+        sudo useradd -m -d /var/lib/pipelines -s /bin/false pipelines
     fi
 
     echo "Creating workspace..."
-    `mkdir -p /var/lib/pipelines/workspace > /dev/null`
-    `chown -R pipelines. /var/lib/pipelines > /dev/null`
+    sudo mkdir -p /var/lib/pipelines/workspace
+    sudo chown -R pipelines:pipelines /var/lib/pipelines
 
-    echo `pipelines --version`
+    pipelines --version
+
     echo "Successfully installed"
 }
 
