3003c89f5f0d28fbddd741b723f2098d78085406
xuqingfeng <js-xqf@hotmail.com>
2016-12-07 13:32:22 +0800

    Update install.sh - choose which version to install - create ssh key

--- install.sh
+++ install.sh
@@ -6,25 +6,43 @@ install_pipelines(){
 
     echo "Checking dependencies..."
     if [ ! -n "$(which python)" ]; then
-        echo "Need python installed !";
-        exit 1;
+        echo "Need python installed !"
+        exit 1
     fi
     if [ ! -n "$(which pip)" ]; then
-        echo "Need pip installed !";
-        exit 1;
+        echo "Need pip installed !"
+        exit 1
     fi
     if [ ! -n "$(which git)" ]; then
         echo "Need git installed !"
-        exit 1;
+        exit 1
     fi
 
     echo "Installing pipelines..."
-    sudo pip install git+git://github.com/Wiredcraft/pipelines@dev > /dev/null
+    echo -n "Which version of pipelines to install? (dev/stable)"
+    read version
+    if [ -z "$version" ]; then
+        version=stable
+    fi
+
+    if [ "$version" == "dev" ]; then
+        sudo pip install git+git://github.com/Wiredcraft/pipelines@dev > /dev/null
+    elif [ "$version" == "stable" ]; then
+        sudo pip install pipelines
+    else
+        echo "Invalid version !"
+        exit 1
+    fi
 
     echo "Creating user..."
     id pipelines > /dev/null 2>&1
-    if [ ! $? -eq 0 ]; then
+    if [ $? -nq 0 ]; then
         sudo useradd -m -d /var/lib/pipelines -s /bin/false pipelines
+        sudo -u pipelines ssh-keygen -P '' -f /var/lib/pipelines/.ssh/id_rsa -b 2048 -q
+        echo "Pipelines' SSH public KEY:"
+        echo ""
+        sudo cat /var/lib/pipelines/.ssh/id_rsa.pub
+        echo ""
     fi
 
     echo "Creating workspace..."
