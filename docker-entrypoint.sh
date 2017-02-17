dbc7e8169aac17a379c34aa78b772b44ac9e655f
Vincent Viallet <vincent.viallet@gmail.com>
2017-02-17 17:32:42 +0800

    Fix entrypoint cmd check for pipelines.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,9 @@ set -e
 
 # Using $3 since CMD is expended by shell as
 # entrypoint /bin/sh -c CMD
-if [ "$3" = 'pipelines' -a "$(id -u)" = '0' ]; then
+# Using cut as $3 includes all the params...
+cmd=$(echo $3 | cut -f1 -d' ')
+if [ "$cmd" = 'pipelines' -a "$(id -u)" = '0' ]; then
     chown -R pipelines .
     exec gosu pipelines "$0" "$@"
 fi
