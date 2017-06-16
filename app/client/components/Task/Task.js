861f9e59aa353a6ba07aabc038ed5a29043ed325
Juha <juha@wiredcraft.com>
2017-06-16 18:53:23 +0800

    Fix highlighting for UI.

--- app/client/components/Task/Task.js
+++ app/client/components/Task/Task.js
@@ -420,8 +420,8 @@ export default class Task extends Component {
 
       function highlight(input){
         var timestampRegex = /(\d\d\d\d:\d\d:\d\d) (\d\d:\d\d:\d\d):/g;
-        var failRegex = /([^0] failed|failed|fail|error|err|status: 1)/g;
-        var succRegex = /(0 failed|status: 0)/g;
+        var failRegex = /([1-9] failed|failed|fail|error|err|status: 1)/g;
+        var succRegex = /(0 failed|status: 0|success)/g;
         var bashColors = [
         [/\[(0;)?30m(.*?)\[(0)?m/g, '<span class="black">$2</span>'],
         [/\[(0;)?31m(.*?)\[(0)?m/g, '<span class="red">$2</span>'],
